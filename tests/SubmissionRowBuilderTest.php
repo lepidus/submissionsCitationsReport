ae46cafc83f866bd33c86c2747f66625f5c13989
Jhon <jhon@lepidus.com.br>
2024-07-17 12:57:23 -0400

    Adds unit test to check exporting of SciELO user
    Issue: documentacao-e-tarefas/scielo#680
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- tests/SubmissionRowBuilderTest.php
+++ tests/SubmissionRowBuilderTest.php
@@ -2,43 +2,42 @@
 
 namespace APP\plugins\reports\submissionsCitationsReport\tests;
 
-use APP\server\Server;
 use APP\submission\Submission;
 use APP\publication\Publication;
 use APP\author\Author;
+use APP\core\Application;
+use PKP\core\Core;
 use APP\facades\Repo;
+use PKP\security\Role;
+use PKP\log\event\PKPSubmissionEventLogEntry;
 use Illuminate\Support\LazyCollection;
-use PHPUnit\Framework\TestCase;
+use PKP\tests\DatabaseTestCase;
 use APP\plugins\reports\submissionsCitationsReport\classes\SubmissionRowBuilder;
 
-class SubmissionRowBuilderTest extends TestCase
+class SubmissionRowBuilderTest extends DatabaseTestCase
 {
+    private $contextId = 1;
     private $submission;
-    private $publication;
     private $authors;
-    private $context;
     private $locale = 'en';
 
     public function setUp(): void
     {
-        $this->context = $this->createTestContext();
-        $this->authors = $this->createTestAuthors();
-        $this->submission = $this->createTestSubmission();
-        $this->publication = $this->createTestPublication();
+        parent::setUp();
 
-        $this->submission->setData('publications', [$this->publication]);
-        $this->submission->setData('currentPublicationId', $this->publication->getId());
+        $this->submission = $this->createTestSubmission();
+        $this->editPublication();
     }
 
-    private function createTestContext(): Server
+    public function tearDown(): void
     {
-        $context = new Server();
-        $context->setAllData([
-            'id' => 13,
-            'urlPath' => 'publicknowledge'
-        ]);
+        parent::tearDown();
+        Repo::submission()->delete($this->submission);
+    }
 
-        return $context;
+    protected function getAffectedTables()
+    {
+        return ['users', 'user_groups', 'user_settings', 'user_group_settings', 'user_user_groups', 'event_log'];
     }
 
     private function createTestAuthors(): array
@@ -56,31 +55,31 @@ class SubmissionRowBuilderTest extends TestCase
 
     private function createTestSubmission(): Submission
     {
+        $context = Application::get()->getContextDAO()->getById($this->contextId);
+
         $submission = new Submission();
+        $submission->setData('contextId', $this->contextId);
+        $publication = new Publication();
 
-        $submission->setData('id', 1234);
-        $submission->setData('contextId', $this->context->getId());
+        $submissionId = Repo::submission()->add($submission, $publication, $context);
 
-        return $submission;
+        return Repo::submission()->get($submissionId);
     }
 
-    private function createTestPublication(): Publication
+    private function editPublication()
     {
-        $publication = new Publication();
+        $publication = $this->submission->getCurrentPublication();
         $doiObject = Repo::doi()->newDataObject([
             'doi' => '10.666/949494',
-            'contextId' => $this->context->getId()
+            'contextId' => $this->contextId
         ]);
+        $authors = $this->createTestAuthors();
 
-        $publication->setAllData([
-            'id' => 1314,
-            'submissionId' => $this->submission->getId(),
-            'title' => [$this->locale => 'Advancements in rocket science'],
-            'authors' => $this->lazyCollectionFromAuthors($this->authors),
-            'doiObject' => $doiObject
-        ]);
+        $publication->setData('title', 'Advancements in rocket science', $this->locale);
+        $publication->setData('authors', $this->lazyCollectionFromAuthors($authors));
+        $publication->setData('doiObject', $doiObject);
 
-        return $publication;
+        $this->submission->setData('publications', [$publication]);
     }
 
     private function lazyCollectionFromAuthors(array $authors): LazyCollection
@@ -94,19 +93,91 @@ class SubmissionRowBuilderTest extends TestCase
         return $collectionAuthors;
     }
 
+    private function createSubmitter(): int
+    {
+        $userSubmitter = Repo::user()->newDataObject([
+            'userName' => 'the_godfather',
+            'email' => 'donvito@corleone.com',
+            'password' => 'miaumiau',
+            'country' => 'BR',
+            'givenName' => [$this->locale => 'Don'],
+            'familyName' => [$this->locale => 'Vito Corleone'],
+            'dateRegistered' => Core::getCurrentDate()
+        ]);
+        $userSubmitterId = Repo::user()->dao->insert($userSubmitter);
+
+        $eventLog = Repo::eventLog()->newDataObject([
+            'assocType' => Application::ASSOC_TYPE_SUBMISSION,
+            'assocId' => $this->submission->getId(),
+            'eventType' => PKPSubmissionEventLogEntry::SUBMISSION_LOG_SUBMISSION_SUBMIT,
+            'userId' => $userSubmitterId,
+            'dateLogged' => Core::getCurrentDate()
+        ]);
+        Repo::eventLog()->add($eventLog);
+
+        return $userSubmitterId;
+    }
+
+    private function createScieloJournalUserGroup(): int
+    {
+        $scieloJournalUserGroup = Repo::userGroup()->newDataObject([
+            'name' => [
+                'en' => 'SciELO Journal',
+                'es' => 'Revista SciELO',
+                'pt_BR' => 'Periódico SciELO'
+            ],
+            'abbrev' => [
+                'en' => 'SciELO',
+                'es' => 'SciELO',
+                'pt_BR' => 'SciELO'
+            ],
+            'roleId' => Role::ROLE_ID_SUB_EDITOR,
+            'contextId' => $this->contextId
+        ]);
+
+        return Repo::userGroup()->add($scieloJournalUserGroup);
+    }
+
     public function testBuildSubmissionRow(): void
     {
         $rowBuilder = new SubmissionRowBuilder();
+        $context = Application::get()->getContextDAO()->getById($this->contextId);
         $submissionId = $this->submission->getId();
+        $publication = $this->submission->getCurrentPublication();
 
         $expectedRow = [
             $submissionId,
-            $this->publication->getLocalizedFullTitle($this->locale),
+            $publication->getLocalizedFullTitle($this->locale),
             'Bernard Summer; Gillian Gilbert',
             "https://pkp.sfu.ca/ops/index.php/publicknowledge/workflow/access/$submissionId",
-            '10.666/949494'
+            '10.666/949494',
         ];
 
-        $this->assertEquals($expectedRow, $rowBuilder->buildRow($this->context, $this->submission));
+        $this->assertEquals($expectedRow, $rowBuilder->buildRow($context, $this->submission));
+    }
+
+    public function testBuildSubmissionRowWithScieloJournal(): void
+    {
+        $submitterId = $this->createSubmitter();
+        $scieloJournalGroupId = $this->createScieloJournalUserGroup();
+        Repo::userGroup()->assignUserToGroup(
+            $submitterId,
+            $scieloJournalGroupId
+        );
+
+        $rowBuilder = new SubmissionRowBuilder();
+        $context = Application::get()->getContextDAO()->getById($this->contextId);
+        $submissionId = $this->submission->getId();
+        $publication = $this->submission->getCurrentPublication();
+        $expectedRow = [
+            $submissionId,
+            $publication->getLocalizedFullTitle($this->locale),
+            'Bernard Summer; Gillian Gilbert',
+            "https://pkp.sfu.ca/ops/index.php/publicknowledge/workflow/access/$submissionId",
+            '10.666/949494',
+            __('common.yes')
+        ];
+
+        $this->assertEquals($expectedRow, $rowBuilder->buildRow($context, $this->submission));
     }
 }
