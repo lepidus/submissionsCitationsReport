448c3e71dd76ea29a2ab5892f6e9a483d4ab4161
Jhon <jhon@lepidus.com.br>
2024-07-23 16:30:03 -0400

    Implements cache to store submissions with citations
    Issue: documentacao-e-tarefas/scielo#680
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/SubmissionsCitationsReportBuilder.php
+++ classes/SubmissionsCitationsReportBuilder.php
@@ -2,6 +2,7 @@
 
 namespace APP\plugins\reports\submissionsCitationsReport\classes;
 
+use PKP\cache\CacheManager;
 use APP\facades\Repo;
 use APP\submission\Submission;
 use APP\plugins\reports\submissionsCitationsReport\classes\SubmissionsCitationsReport;
@@ -11,9 +12,45 @@ class SubmissionsCitationsReportBuilder
 {
     public function createReport($context): SubmissionsCitationsReport
     {
+        $submissionsWithCitations = $this->getSubmissionsWithCitations($context->getId());
+        $report = new SubmissionsCitationsReport($context->getId(), $submissionsWithCitations);
+
+        return $report;
+    }
+
+    private function getSubmissionsWithCitations(int $contextId): array
+    {
+        $cacheManager = CacheManager::getManager();
+        $cache = $cacheManager->getFileCache(
+            $contextId,
+            'submissions_with_citations',
+            [$this, 'cacheDismiss']
+        );
+
+        $submissionsIds = $cache->getContents();
+        if (is_null($submissionsIds)) {
+            $cache->flush();
+
+            $submissionsWithCitations = $this->retrieveSubmissionsWithCitations($contextId);
+            $submissionsIds = array_keys($submissionsWithCitations);
+            $cache->setEntireCache($submissionsIds);
+
+            return $submissionsWithCitations;
+        }
+
+        $submissionsWithCitations = [];
+        foreach ($submissionsIds as $submissionId) {
+            $submissionsWithCitations[] = Repo::submission()->get($submissionId);
+        }
+
+        return $submissionsWithCitations;
+    }
+
+    private function retrieveSubmissionsWithCitations(int $contextId): array
+    {
         $collector = Repo::submission()->getCollector();
         $submissions = $collector
-            ->filterByContextIds([$context->getId()])
+            ->filterByContextIds([$contextId])
             ->filterByStatus([Submission::STATUS_PUBLISHED])
             ->orderBy($collector::ORDERBY_DATE_SUBMITTED, $collector::ORDER_DIR_ASC)
             ->getMany()
@@ -25,12 +62,15 @@ class SubmissionsCitationsReportBuilder
         $submissionsWithCitations = [];
         foreach ($submissions as $submission) {
             if ($submissionsCitationsCount[$submission->getId()] > 0) {
-                $submissionsWithCitations[] = $submission;
+                $submissionsWithCitations[$submission->getId()] = $submission;
             }
         }
 
-        $report = new SubmissionsCitationsReport($context->getId(), $submissionsWithCitations);
+        return $submissionsWithCitations;
+    }
 
-        return $report;
+    public function cacheDismiss()
+    {
+        return null;
     }
 }
